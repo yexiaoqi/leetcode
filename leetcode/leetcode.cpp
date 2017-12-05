4a2e68731d2cbe10966a816aa64b87d4cd398c39
yexiaoqi <2417192079@qq.com>
2017-12-05 18:28:09 +0800

    26. Remove Duplicates from Sorted Array

--- leetcode/leetcode.cpp
+++ leetcode/leetcode.cpp
@@ -1,11 +1,23 @@
-// leetcode.cpp : 定义控制台应用程序的入口点。
-//
+//Given a sorted array, remove the duplicates in-place such that each element appear
+//only once and return the new length.
 
-#include "stdafx.h"
-
-
-int main()
+//Do not allocate extra space for another array, you must do this by modifying the
+//input array in - place with O(1) extra memory.
+class Solution
 {
-    return 0;
-}
+public:
+	int removeDuplicates(vector<int>& nums)
+	{
+		if (nums.empty())return 0;
+		int index = 0;
+		for (int i = 1;i<nums.size();++i)
+		{
+			if (nums[index] != nums[i])
+			{
+				nums[++index] = nums[i];
+			}
+		}
+		return index + 1;
+	}
+};
 
