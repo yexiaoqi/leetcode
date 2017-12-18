feb5fe0da2a1119e1cc701b355b3427fef9ec1d3
yexiaoqi <2417192079@qq.com>
2017-12-18 18:19:14 +0800

    60. Permutation Sequence

---
+++ leetcode/60. Permutation Sequence.cpp
@@ -0,0 +1,41 @@
+//The set[1, 2, 3, …, n] contains a total of n!unique permutations.
+//
+//By listing and labeling all of the permutations in order,
+//We get the following sequence(ie, for n = 3) :
+//
+//	"123"
+//	"132"
+//	"213"
+//	"231"
+//	"312"
+//	"321"
+//	Given n and k, return the kth permutation sequence.
+//
+//	Note: Given n will be between 1 and 9 inclusive.
+
+
+class Solution
+{
+public:
+	string getPermutation(int n, int k)
+	{
+		int ptable[10] = { 1 };
+		for (int i = 1;i<n;++i)
+		{
+			ptable[i] = i*ptable[i - 1];
+		}
+		vector<char> numset = { '1','2','3','4','5','6','7','8','9' };
+		string result;
+		while (n>0)
+			//不能是for(int i=0;i<=n;++i)，因为--n，n会变，但我们其实就想执行最初的n+1次
+		{
+			int temp;
+			temp = (k - 1) / ptable[n - 1];
+			result += numset[temp];
+			numset.erase(numset.begin() + temp);
+			k = k - temp*ptable[n - 1];
+			--n;
+		}
+		return result;
+	}
+};
