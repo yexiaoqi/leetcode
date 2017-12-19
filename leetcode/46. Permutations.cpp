0a41550beca9878cc1c5593a2853d906f9b2c171
yexiaoqi <2417192079@qq.com>
2017-12-19 21:34:33 +0800

    46. Permutations

---
+++ leetcode/46. Permutations.cpp
@@ -0,0 +1,62 @@
+//Given a collection of distinct numbers, return all possible permutations.
+//
+//For example,
+//[1, 2, 3] have the following permutations :
+//[
+//	[1, 2, 3],
+//	[1, 3, 2],
+//	[2, 1, 3],
+//	[2, 3, 1],
+//	[3, 1, 2],
+//	[3, 2, 1]
+//]
+//方法1.传引用
+class Solution
+{
+public:
+	vector<vector<int>> permute(vector<int>& nums)
+	{
+		vector<vector<int>>result;
+		dfs(0, nums, result);
+		return result;
+	}
+	void dfs(int pos, vector<int>&nums, vector<vector<int>> &result)
+	{
+		if (pos == nums.size())
+		{
+			result.push_back(nums);
+			return;
+		}
+		for (int i = pos;i<nums.size();++i)
+		{
+			swap(nums[pos], nums[i]);//for循环将pos~end中的每个数放到pos位置中去
+			dfs(pos + 1, nums, result);//假设pos位置确定，那么对pos+1~end中的数继续递归
+			swap(nums[pos], nums[i]);
+		}
+	}
+};
+//方法2.传值
+class Solution 
+{
+public:
+	vector<vector<int>> permute(vector<int>& nums)
+	{
+		vector<vector<int>>result;
+		dfs(0, nums, result);
+		return result;
+	}
+	void dfs(int pos, vector<int>nums, vector<vector<int>> &result)
+	{
+		if (pos == nums.size())
+		{
+			result.push_back(nums);
+			return;
+		}
+		for (int i = pos;i<nums.size();++i)
+		{
+			swap(nums[pos], nums[i]);//for循环将pos~end中的每个数放到pos位置中去
+			dfs(pos + 1, nums, result);//假设pos位置确定，那么对pos+1~end中的数继续递归
+									   //swap(nums[pos],nums[i]);
+		}
+	}
+};
