0c0db005aa943ce080d36df903a4308194b8ca6b
yexiaoqi <2417192079@qq.com>
2017-12-28 19:45:27 +0800

    86. Partition List

---
+++ leetcode/86. Partition List.cpp
@@ -0,0 +1,42 @@
+//Given a linked list and a value x, partition it such that all nodes less than x come before nodes greater than or equal to x.
+//
+//You should preserve the original relative order of the nodes in each of the two partitions.
+//
+//For example,
+//Given 1->4->3->2->5->2 and x = 3,
+//return 1->2->2->4->3->5.
+
+/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode(int x) : val(x), next(NULL) {}
+* };
+*/
+//题目要求对链表分段，所有小于X的元素都排在大于等于X的前面。
+class Solution
+{
+public:
+	ListNode* partition(ListNode* head, int x)
+	{
+		ListNode *node1 = new ListNode(0);
+		ListNode *node2 = new ListNode(0);
+		ListNode *p1 = node1, *p2 = node2;
+		while (head)
+		{
+			if (head->val<x)
+			{
+				p1 = p1->next = head;
+			}
+			else
+			{
+				p2 = p2->next = head;
+			}
+			head = head->next;
+		}
+		p2->next = NULL;
+		p1->next = node2->next;
+		return node1->next;
+	}
+};
