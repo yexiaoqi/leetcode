2adde5460f018d96c6f67061a636834fae58adba
yexiaoqi <yexiaoqi17@gmail.com>
2019-09-03 19:28:41 +0800

    26和80更新

--- leetcode/26. Remove Duplicates from Sorted Array.cpp
+++ leetcode/26. Remove Duplicates from Sorted Array.cpp
@@ -42,3 +42,32 @@ public:
 		return distance(nums.begin(), unique(nums.begin(), nums.end()));
 	}
 };
+
+//solution 3  这个方法易懂以扩展
+//看到原地操作一般会想到指针，这个题目需要双指针。
+//使用快慢指针来记录遍历的坐标，最开始时两个指针都指向第一个数字，如果两个指针指的数字相同，则快指针向前走一步，
+//如果不同，则两个指针都向前走一步，这样当快指针走完整个数组后，慢指针当前的坐标加1就是数组中不同数字的个数
+class Solution {
+public:
+	int removeDuplicates(vector<int>& nums) {
+
+		if (nums.empty())
+		{
+			return 0;
+		}
+		int pre = 0, cur = 0, n = nums.size();
+		while (cur<n)
+		{
+			if (nums[pre] == nums[cur])
+			{
+				++cur;
+			}
+			else
+			{
+				nums[++pre] = nums[cur++];
+			}
+		}
+		return pre + 1;
+
+	}
+};
--- leetcode/80. Remove Duplicates from Sorted Array II.cpp
+++ leetcode/80. Remove Duplicates from Sorted Array II.cpp
@@ -22,3 +22,40 @@ public:
 		return index;
 	}
 };
+
+//solution 2
+//易懂
+//这里允许最多重复的次数是两次，那么我们就需要用一个变量count来记录还允许有几次重复，count初始化为1，
+//如果出现过一次重复，则count递减1，那么下次再出现重复，快指针直接前进一步，如果这时候不是重复的，则count恢复1，
+//由于整个数组是有序的，所以一旦出现不重复的数，则一定比这个数大，此数之后不会再有重复项
+class Solution {
+public:
+	int removeDuplicates(vector<int>& nums) {
+		int n = nums.size();
+		if (n <= 2)
+		{
+			return n;
+		}
+		int pre = 0, cur = 1, count = 1;
+		while (cur<n)
+		{
+			if (nums[cur] == nums[pre] && count == 0)
+			{
+				++cur;
+			}
+			else
+			{
+				if (nums[cur] == nums[pre])
+				{
+					--count;
+				}
+				else
+				{
+					count = 1;
+				}
+				nums[++pre] = nums[cur++];
+			}
+		}
+		return pre + 1;
+	}
+};
