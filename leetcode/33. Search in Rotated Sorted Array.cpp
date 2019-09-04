635f123962f0776abed4b33f6f0b249db77cb1d2
yexiaoqi <yexiaoqi17@gmail.com>
2019-09-04 20:59:32 +0800

    33再整理

--- leetcode/33. Search in Rotated Sorted Array.cpp
+++ leetcode/33. Search in Rotated Sorted Array.cpp
@@ -6,6 +6,10 @@
 //
 //You may assume no duplicate exists in the array.
 
+//https://www.cnblogs.com/grandyang/p/4325648.html
+//二分搜索法的关键在于获得了中间数后，判断下面要搜索左半段还是右半段，我们观察上面红色的数字都是升序的，
+//由此我们可以观察出规律，如果中间的数小于最右边的数，则右半段是有序的，若中间数大于最右边数，则左半段是有序的，
+//我们只要在有序的半段里用首尾两个数组来判断目标值是否在这一区域内，这样就可以确定保留哪半边了
 class Solution
 {
 public:
