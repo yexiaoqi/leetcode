eadab2592525cea73f18374876d820195335cf8d
yexiaoqi <yexiaoqi17@gmail.com>
2019-09-10 12:46:18 +0800

    60更新

--- leetcode/60. Permutation Sequence.cpp
+++ leetcode/60. Permutation Sequence.cpp
@@ -39,3 +39,27 @@ public:
 		return result;
 	}
 };
+
+
+//https://www.cnblogs.com/grandyang/p/4358678.html
+class Solution {
+public:
+	string getPermutation(int n, int k) {
+		string res;
+		string nums = "123456789";
+		vector<int> f(n, 1);
+		for (int i = 1; i<n; ++i)
+		{
+			f[i] = f[i - 1] * i;
+		}
+		--k;
+		for (int i = n; i >= 1; --i)
+		{
+			int j = k / f[i - 1];
+			k %= f[i - 1];
+			res.push_back(nums[j]);
+			nums.erase(j, 1);
+		}
+		return res;
+	}
+};
