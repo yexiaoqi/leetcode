fd2c1aa7cb7f2d972c3b63951694b04f813b6f14
yexiaoqi <yexiaoqi17@gmail.com>
2019-09-10 14:31:55 +0800

    36. Valid Sudoku更改

--- leetcode/36. Valid Sudoku.cpp
+++ leetcode/36. Valid Sudoku.cpp
@@ -36,3 +36,32 @@ public:
 		return true;
 	}
 };
+
+//https://www.cnblogs.com/grandyang/p/4421217.html
+class Solution {
+public:
+	bool isValidSudoku(vector<vector<char>>& board) {
+		vector<vector<bool>> row(9, vector<bool>(9));
+		vector<vector<bool>> col(9, vector<bool>(9));
+		vector<vector<bool>> box(9, vector<bool>(9));
+		for (int i = 0; i<9; ++i)
+		{
+			for (int j = 0; j<9; ++j)
+			{
+				if (board[i][j] == '.')
+				{
+					continue;
+				}
+				int c = board[i][j] - '1';
+				if (row[i][c] || col[c][j] || box[3 * (i / 3) + j / 3][c])
+				{
+					return false;
+				}
+				row[i][c] = true;
+				col[c][j] = true;
+				box[3 * (i / 3) + j / 3][c] = true;
+			}
+		}
+		return true;
+	}
+};
