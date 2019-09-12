592eb5f2e968b76982fb64c7eeaa142b041b5068
yexiaoqi <yexiaoqi17@gmail.com>
2019-09-12 11:37:46 +0800

    2更改

--- leetcode/2. Add Two Numbers.cpp
+++ leetcode/2. Add Two Numbers.cpp
@@ -43,3 +43,42 @@ public:
 		return StackAnchor.next;
 	}
 };
+
+https://www.cnblogs.com/grandyang/p/4129891.html
+/**
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode(int x) : val(x), next(NULL) {}
+* };
+*/
+class Solution {
+public:
+	ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+		ListNode *dummy = new ListNode(-1), *current = dummy;
+		int carry = 0;
+		while (l1 || l2)
+		{
+			int val1 = l1 ? l1->val : 0;
+			int val2 = l2 ? l2->val : 0;
+			int sum = val1 + val2 + carry;
+			carry = sum / 10;
+			current->next = new ListNode(sum % 10);
+			current = current->next;
+			if (l1)
+			{
+				l1 = l1->next;
+			}
+			if (l2)
+			{
+				l2 = l2->next;
+			}
+		}
+		if (carry)
+		{
+			current->next = new ListNode(1);
+		}
+		return dummy->next;
+	}
+};
