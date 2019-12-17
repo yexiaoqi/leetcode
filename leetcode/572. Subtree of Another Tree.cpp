66aada5251f78f27bdda0c730de4c04bf165820b
yexiaoqi <yexiaoqi17@gmail.com>
2019-12-17 09:47:51 +0800

    浮点数判断是否相等不能用==

--- leetcode/572. Subtree of Another Tree.cpp
+++ leetcode/572. Subtree of Another Tree.cpp
@@ -9,35 +9,50 @@
 */
 class Solution {
 public:
-	bool isSubtree(TreeNode* pRoot1, TreeNode* pRoot2) {
-
-		if (!pRoot1)
-		{
-			return false;
-		}
-		if (issame(pRoot1, pRoot2))
+	bool Equal(double num1, double num2)
+	{
+		if (abs(num1 - num2)<0.0000001)
 		{
 			return true;
 		}
-		return isSubtree(pRoot1->left, pRoot2) || isSubtree(pRoot1->right, pRoot2);
+		return false;
+	}//注意判断两个浮点数是否相等时，不能直接写成==，因为计算机内表示小数有误差
+	bool HasSubtree(TreeNode* pRoot1, TreeNode* pRoot2)
+	{
+		bool res = false;
+
+		if (pRoot1&&pRoot2)
+		{
+			if (Equal(pRoot1->val, pRoot2->val))
+			{
+				res = issame(pRoot1, pRoot2);
+			}
+			if (!res)
+			{
+				res = HasSubtree(pRoot1->left, pRoot2);
+			}
+			if (!res)
+			{
+				res = HasSubtree(pRoot1->right, pRoot2);
+			}
+		}
+		return res;
 	}
 	bool issame(TreeNode* s, TreeNode* t)
 	{
-		if (!s && !t)
+		if (!t)
 		{
 			return true;
 		}
-		if (!s || !t)
+		if (!s)
 		{
 			return false;
 		}
-		if (s->val != t->val)
+		if (!Equal(s->val, t->val))
 		{
 			return false;
 		}
 		return issame(s->left, t->left) && issame(s->right, t->right);
 	}
-
 };
-
 
