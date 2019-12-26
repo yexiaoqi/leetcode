e5327b8a8e434270fc51ccb44abfed434b46c0ea
yexiaoqi <yexiaoqi17@gmail.com>
2019-12-26 13:22:45 +0800

    只是把上面的char改为string，结果对，但内存超了，这是为啥

--- leetcode/jingzhioffer_43_NumberOf1.cpp
+++ leetcode/jingzhioffer_43_NumberOf1.cpp
@@ -39,3 +39,42 @@ public:
         return numfirst+numother+numrecur;
     }
 };
+
+//只是把上面的char改为string，结果对，但内存超了，这是为啥
+class Solution {
+public:
+    int NumberOf1Between1AndN_Solution(int n)
+    {
+        string strs=to_string(n);
+        return numberof1(strs);
+    }
+    int numberof1(string &str)
+    {
+        int first=str[0]-'0';
+        int len=str.size();
+        int numfirst=0;
+        if(len==1&&first==0)
+        {
+            return 0;
+        }
+        if(len==1&&first>=1)
+        {
+            return 1;
+        }
+        if(first>1)
+        {
+            numfirst=pow(10,len-1);
+             str.erase(0,1);
+        }
+        else if(first==1)
+        {
+            str.erase(0,1);
+            numfirst=stoi(str)+1;
+        }
+        int numother=0;
+        numother=first*(len-1)*pow(10,len-2);
+        int numrecur=0;
+         numrecur=numberof1(str);
+        return numfirst+numother+numrecur;
+    }
+};
