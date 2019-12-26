b62891fa7ddb9d4262f9cfc0e05bd55ba0276cc9
yexiaoqi <yexiaoqi17@gmail.com>
2019-12-26 14:01:00 +0800

    整数中1出现的次数方法二

--- leetcode/jingzhioffer_43_NumberOf1.cpp
+++ leetcode/jingzhioffer_43_NumberOf1.cpp
@@ -78,3 +78,31 @@ public:
         return numfirst+numother+numrecur;
     }
 };
+
+//方法二https://www.nowcoder.com/profile/3371548/codeBookDetail?submissionId=16319486
+class Solution {
+public:
+    int NumberOf1Between1AndN_Solution(int n)
+    {
+        int cnt=0;
+        long long i=1;
+        for(;i<=n;i*=10)
+        {
+            int a=n/i;
+            int b=n%i;
+            if(a%10==0)
+            {
+                cnt+=a/10*i;
+            }
+            else if(a%10==1)
+            {
+                cnt+=a/10*i+b+1;
+            }
+            else{
+                cnt+=(a/10+1)*i;
+            }
+        }
+        return cnt;
+    }
+   
+};
