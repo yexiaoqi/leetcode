392bef7c232ce47e1b3a008c6efa183dfefe7f89
yexiaoqi <yexiaoqi17@gmail.com>
2019-12-26 11:09:09 +0800

    整数中1出现的次数

---
+++ leetcode/jingzhioffer_43_NumberOf1.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int NumberOf1Between1AndN_Solution(int n)
+    {
+        char str[50];
+        sprintf(str,"%d",n);
+        int res=numberof1(str);
+        return res;
+    }
+    int numberof1(char str[])
+    {
+        if(!str||*str<'0'||*str>'9'||*str=='\0')
+        {
+            return 0;
+        }
+        int first=*str-'0';
+        int numfirst=0;
+        int len=strlen(str);
+        if(len==1&&first==0)
+        {
+            return 0;
+        }
+        if(len==1&&first>=1)
+        {
+            return 1;
+        }
+        if(first>1)
+        {
+            numfirst=pow(10,len-1);
+        }
+        else if(first==1)
+        {
+            numfirst=atoi(str+1)+1;
+        }
+        int numother=0;
+        numother=first*(len-1)*pow(10,len-2);
+        int numrecur=0;
+        numrecur=numberof1(str+1);
+        return numfirst+numother+numrecur;
+    }
+};
