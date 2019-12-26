2f5a7e32eccd071c40d0cb233efd8df3b9c0127a
yexiaoqi <yexiaoqi17@gmail.com>
2019-12-26 12:48:45 +0800

    把数组排成最小的数

---
+++ leetcode/jiznhioffer_45_SortArrayForMinNumber.cpp
@@ -0,0 +1,27 @@
+bool compare(string a,string b)
+{
+    string str1=a+b;
+    string str2=b+a;
+    return str1<str2;
+}
+class Solution {
+public:
+    string PrintMinNumber(vector<int> numbers) {
+        string res;
+        vector<string>  strs;
+        if(numbers.size()==0)
+        {
+            return res;
+        }
+        for(int i=0;i<numbers.size();++i)
+        {
+            strs.push_back(to_string(numbers[i]));//数字转字符串to_string
+        }
+        sort(strs.begin(),strs.end(),compare);
+        for(int i=0;i<strs.size();++i)
+        {
+            res=res+strs[i];
+        }
+        return res;
+    }
+};
