d694dd111ee50c0f8cf28edfa5b855662defb71d
yexiaoqi <yexiaoqi17@gmail.com>
2019-12-30 13:06:24 +0800

    翻转单词顺序列

---
+++ leetcode/jianzhioffer_58_01_ReverseWordsInSentence.cpp
@@ -0,0 +1,33 @@
+//同leetcode151
+class Solution {
+public:
+    void reverse(string &str,int begin,int end)
+    {
+       while(begin<end)
+       {
+           swap(str[begin++],str[end--]);
+       }
+    }
+    string ReverseSentence(string str) {
+       int begin=0;
+        int end=str.size()-1;
+        reverse(str,begin,end);
+        int i=0;
+        while(i<=end)
+        {
+            while(i<=end&&str[i]==' ')
+            {
+                i++;
+            }
+            int b=i;
+            int e=i;
+            while(i<=end&&str[i]!=' ')
+            {
+                i++;
+                e++;
+            }
+            reverse(str,b,e-1);
+        }
+        return str;
+    }
+};
