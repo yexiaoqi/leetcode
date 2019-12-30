b0cf2c831388d3db9a0e12c553c44e4684c4e089
yexiaoqi <yexiaoqi17@gmail.com>
2019-12-30 09:49:16 +0800

    和为s的两个数字更新

--- leetcode/jianzhioffer_57_01_TwoNumbersWithSum.cpp
+++ leetcode/jianzhioffer_57_01_TwoNumbersWithSum.cpp
@@ -1,3 +1,4 @@
+//自己的方法，空间复杂度o(n),时间复杂度o(n)
 class Solution {
 public:
 	vector<int> FindNumbersWithSum(vector<int> array, int sum) {
@@ -19,4 +20,36 @@ public:
 		}
 		return res;
 	}
+};
+
+//空间复杂度o(1),时间复杂度o(n)
+class Solution {
+public:
+    vector<int> FindNumbersWithSum(vector<int> array,int sum) {
+        vector<int> res;
+        if(array.size()<=1)
+        {
+            return res;
+        }
+        int begin=0;
+        int end=array.size()-1;
+        while(begin<end)
+        {
+            int sum2=array[begin]+array[end];
+            if(sum2==sum)
+            {
+                res.push_back(array[begin]);
+                res.push_back(array[end]);
+                return res;
+            }
+            else if(sum2<sum)
+            {
+                ++begin;
+            }
+            else{
+                --end;
+            }
+        }
+        return res;
+    }
 };
